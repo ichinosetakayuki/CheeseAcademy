ac5dbc8bd11be0f6fbd25bddffa85351b8ac2df1
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2025-05-17 15:50:27 +0900

    チーズアカデミーの登録

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// jsを記述する際はここに記載していく
+
+  // const toTopBtn = document.getElementById("toTopBtn");
+
+  // window.addEventListener("scroll", () => {
+  //   if (window.scrollY > 1200) {
+  //     toTopBtn.classList.add("show");
+  //   } else {
+  //     toTopBtn.classList.remove("show");
+  //   }
+  // });
+
